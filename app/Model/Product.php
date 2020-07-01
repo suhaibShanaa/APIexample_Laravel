ddf098541d140cf1d49f8be507c48855424096c5
Suhaib-ANE <suhaibShanaa>
2020-07-01 20:46:39 +0300

    add Resource , Collection and retricve this and install postman to show this from apis

---
+++ app/Model/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        //
+
+
+        'name'      => $faker->word,
+        'detail'    =>$faker->paragraph,
+        'price'     =>$faker->numberBetween(100,1000),
+        'stock'     =>$faker->randomDigit(0,5),
+        'discount'  => $faker->numberBetween(2,30)
+    ];
+});
